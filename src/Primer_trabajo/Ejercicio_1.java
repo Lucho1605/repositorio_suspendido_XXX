586dbc8b2b6ca94b1aad81e6c09dc61578e8a167
María Paula <Mara Paula@LAPTOP-2VRDBOCM>
2026-08-27 09:34:57 -0500

    ejercicios y tarea de programación orientada a objetos

---
+++ src/Primer_trabajo/Ejercicio_1.java
@@ -0,0 +1,17 @@
+//
+package Primer_trabajo;
+
+/**
+ *
+ * @author María Paula
+ */
+public class Ejercicio_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
