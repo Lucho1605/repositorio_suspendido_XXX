7027553dcc5f59aea743b50f61d603ffac20c866
Luis Alejandro Acuña-u20261244210@usco.edu.co <Mara Paula@LAPTOP-2VRDBOCM>
2026-08-27 11:56:30 -0500

    se saca el subtutal sin descuento y el total con descuento, dependiendo de que si su valor supera cierta cantidad. codigo solo para una referencia de producto. (ejemplo: venta al por mayor de "packs de cheetos")

--- src/Primer_trabajo/Ejercicio_1.java
+++ src/Primer_trabajo/Ejercicio_1.java
@@ -1,7 +1,11 @@
 //para el primer ejercicio será sobre el proceso que realizan las cajas 
-//registradras automaticamente para implementar descuentos y dar el subtotal
+//registradoras automaticamente de una empresa de venta de artivulos al por 
+//por mayor. implementar descuentos y dar el subtotal
 //y total de la compra.
 package Primer_trabajo;
+
+import static java.time.temporal.TemporalAdjusters.next;
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +18,29 @@ public class Ejercicio_1 {
      * 20261244210
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int cant,prec,subt;
+        double des = 0,tot ; 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ingrese la cantidad de productos a comprar: ");
+        cant= sc.nextInt (); 
+        
+        System.out.println("ingrese el valor de unidad del producto: ");
+        prec = (int) sc.nextLong(); 
+        subt= (int) (cant*prec); 
+        System.out.println("el valor a pagar sin descuento: "+subt);
+        
+        if (subt<15000){
+            des= subt ;
+           
+        }
+        else if (subt>20000){
+            des= subt*0.1;
+       
     }
-    
+        else if (subt>70000){
+            des= subt*0.3; 
+        }
+        tot= subt-des; 
+        System.out.println("el total de su compra es de: "+tot);
+    }
 }
